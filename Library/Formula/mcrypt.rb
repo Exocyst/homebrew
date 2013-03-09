a632d20e17137624544162fd06247937ee56d499
Exocyst <brian.bodemann@gmail.com>
2013-03-08 19:13:35 -0600

    Moved mcrypt to libmcrypt so that the command line tool mcrypt can be added.

--- Library/Formula/mcrypt.rb
+++ Library/Formula/mcrypt.rb
@@ -1,17 +1,38 @@
 require 'formula'
 
 class Mcrypt < Formula
-  homepage 'http://mcrypt.sourceforge.net'
-  url 'http://sourceforge.net/projects/mcrypt/files/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz'
-  sha1 '9a426532e9087dd7737aabccff8b91abf9151a7a'
+  homepage 'http://mcrypt.sourceforge.net/'
+  url 'http://sourceforge.net/projects/mcrypt/files/MCrypt/2.6.8/mcrypt-2.6.8.tar.gz'
+  sha1 '8ae0e866714fbbb96a0a6fa9f099089dc93f1d86'
 
-  option :universal
+  depends_on 'libmcrypt'
+  depends_on 'mhash'
+
+  def patches
+    # fixes outdated dependency on malloc.h
+    DATA
+  end
 
   def install
-    ENV.universal_binary if build.universal?
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
+    system "./configure", "--prefix=#{prefix}",
+                          "CMAKE_LIBRARY_PATH=/usr/local/lib",
+                          "CMAKE_INCLUDE_PATH=/usr/local/include",
                           "--mandir=#{man}"
     system "make install"
   end
 end
+
+__END__
+diff --git a/src/rfc2440.c b/src/rfc2440.c
+index 5a1f296..a33e2b6 100644
+--- a/src/rfc2440.c
++++ b/src/rfc2440.c
+@@ -23,7 +23,7 @@
+ #include <zlib.h>
+ #endif
+ #include <stdio.h>
+-#include <malloc.h>
++// #include <malloc.h>
+
+ #include "xmalloc.h"
+ #include "keys.h"
